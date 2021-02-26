6a3148ff6f432b2c3c0ee9995055eaa395a28a00
Alexander Giesa <alex@77g.de>
2021-02-26 21:35:16 +0100

    feat:(internal/cfg/cfg.go)config file integration

--- internal/cfg/cfg.go
+++ internal/cfg/cfg.go
@@ -1,21 +1,48 @@
 package cfg
 
 import (
-	"log"
+	"io/ioutil"
 	"os"
+
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
 
 	"github.com/gobc/internal/scm"
 )
 
+type option struct {
+	github struct {
+		enable bool   `yaml:"enable"`
+		auth   string `yaml:"auth"`
+	}
+	gitlab struct {
+		enable bool   `yaml:"enable"`
+		auth   string `yaml:"auth"`
+	}
+}
+
 func LoadCfg() {
+
+	o := option{}
 	path, _ := scm.GitRoot()
 
-	if _, err := os.Stat(path + "/.betterconfig"); err == nil {
-		log.Println("existiert")
+	if _, err := os.Stat(path + ".betterconfig"); err == nil {
+		log.Debugln(".betterconfig existiert")
 	} else if os.IsNotExist(err) {
-		log.Println("existiert nicht")
+		log.Debugln(".betterconfig existiert nicht")
 	} else {
 		// Schrodinger: file may or may not exist. See err for details.
-		log.Println("existiert oder ?")
+		log.Debugln("existiert oder ?")
 	}
+
+	file, err := ioutil.ReadFile(".betterconfig.yml")
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	err = yaml.Unmarshal(file, &o)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	log.Debugf("%+v", o)
 }
